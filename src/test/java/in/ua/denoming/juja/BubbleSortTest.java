c5b2275d187140bac5639d894299beb7b5c74a77
karz0n <d.asauliak@gmail.com>
2017-03-06 22:17:34 +0200

    Create sort abstract class and move specific bubble sort algorithm to separate class

---
+++ src/test/java/in/ua/denoming/juja/BubbleSortTest.java
@@ -0,0 +1,34 @@
+package in.ua.denoming.juja;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import in.ua.denoming.juja.algorithms.BubbleSorter;
+
+/**
+ * User: DENIS
+ * Data: 06.03.2017
+ * Time: 20:59
+ */
+public class BubbleSortTest extends SortTest {
+    private Sortable algorithm;
+    private Array<Integer> dataToSort;
+    private ArrayRecorder<Integer> recorder;
+
+    @BeforeEach
+    void setUp() {
+        algorithm = new BubbleSorter();
+        dataToSort = getDataForTesting(DEFAULT_MAX_COUNT, DEFAULT_MAX_VALUE);
+        recorder = new BasicArrayRecorder<>(dataToSort);
+    }
+
+    @Test
+    void testSort() {
+        recorder.startRecord();
+        algorithm.sort(recorder);
+        recorder.stopRecord();
+        print(algorithm, recorder);
+        assertTrue(checkSortOrder(dataToSort));
+    }
+}
--- src/test/java/in/ua/denoming/juja/SortTest.java
+++ src/test/java/in/ua/denoming/juja/SortTest.java
@@ -1,9 +1,5 @@
 package in.ua.denoming.juja;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
-
-import in.ua.denoming.juja.algorithms.BubbleSorter;
 import java.util.Random;
 
 /**
@@ -11,30 +7,11 @@ import java.util.Random;
  * Data: 03.03.2017
  * Time: 21:19
  */
-class SortTest {
-    private static final int DEFAULT_MAX_VALUE = 10000000;
-    private static final int DEFAULT_MAX_COUNT = 10000;
+abstract class SortTest {
+    static final int DEFAULT_MAX_VALUE = 10000000;
+    static final int DEFAULT_MAX_COUNT = 10000;
 
-    @Test
-    public void testAll() {
-        Array<Integer> dataToSort = getDataForTesting(DEFAULT_MAX_COUNT, DEFAULT_MAX_VALUE);
-
-        test(dataToSort, new BubbleSorter());
-    }
-
-    private void test(Array<Integer> dataToSort, Sortable algorithm) {
-        Array<Integer> copy = copyArray(dataToSort);
-
-        ArrayRecorder<Integer> recorder = new BasicArrayRecorder<>(copy);
-        recorder.startRecord();
-        algorithm.sort(recorder);
-        recorder.stopRecord();
-        print(algorithm, recorder);
-
-        assertTrue(checkSortOrder(copy));
-    }
-
-    private BasicArray<Integer> getDataForTesting(int count, int maxValue) {
+    BasicArray<Integer> getDataForTesting(int count, int maxValue) {
         Random engine = new Random();
 
         Integer[] output = new Integer[count];
@@ -45,7 +22,7 @@ class SortTest {
         return new BasicArray<>(output);
     }
 
-    private boolean checkSortOrder(Array<Integer> array) {
+    boolean checkSortOrder(Array<Integer> array) {
         for (int i = 0; i < array.size() - 1; i++) {
             if (array.compareValue(i, i + 1) > 0) {
                 return false;
@@ -54,11 +31,7 @@ class SortTest {
         return true;
     }
 
-    private Array<Integer> copyArray(Array<Integer> array) {
-        return new BasicArray<>(array);
-    }
-
-    private void print(Sortable algorithm, ArrayRecorder<?> recorder) {
+    void print(Sortable algorithm, ArrayRecorder<?> recorder) {
         System.out.println("=========================================================\n");
         System.out.println("Algorithm '" + algorithm.toString() + "' recorded stats:\n");
         System.out.println(recorder.toString());
