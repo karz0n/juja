a7abfb7bc3be065ad61fd139559b35615af71bd4
karz0n <d.asauliak@gmail.com>
2017-04-19 10:38:02 +0300

    Improve implementation of lab26 JuJa course

--- src/main/java/in/ua/denoming/juja/labs/lab24/Book.java
+++ src/main/java/in/ua/denoming/juja/labs/lab24/Book.java
@@ -1,12 +1,9 @@
 package in.ua.denoming.juja.labs.lab24;
 
-import java.util.Objects;
-
 class Book extends Issue {
     private String author;
 
-    @SuppressWarnings("WeakerAccess")
-    public Book(String name, int countPages, String author) {
+    Book(String name, int countPages, String author) {
         super(name, countPages);
 
         this.author = author;
@@ -24,10 +21,6 @@ class Book extends Issue {
     @Override
     public int hashCode() {
         return super.hashCode() + author.hashCode();
-    }
-
-    public String toPrint() {
-        return this.toString();
     }
 
     @Override
